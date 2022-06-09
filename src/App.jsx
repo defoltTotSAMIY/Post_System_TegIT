9752273109fd36b96f0b012f0298da760a60bf09
YusufbekMurodov1210 <murodovyusufbek415@gmail.com>
2022-06-09 14:49:19 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import React , { memo } from "react";
+import React, { memo } from "react";
 import { Input_1 } from "./Components";
-
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
-      <div style={{width: 300 , height: 50 , marginLeft: 100}} >
-        <Input_1 textColor={"#777"} stickColor={"#202020"} placeholder={"Hello"} />
+      <div style={{ width: 300, height: 50, marginLeft: 100 }}>
+        <Input_1
+          textColor={"#777"}
+          stickColor={"#202020"}
+          placeholder={"Hello"}
+        />
       </div>
+      <h1>Hello world</h1>
     </div>
   );
 }
